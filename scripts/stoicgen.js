d0c623fe1e5726a6b167fc28ee4235799a1a047d
GHXXST <53088851+GHXXST@users.noreply.github.com>
2020-10-03 15:19:55 +1000

    Add files via upload

---
+++ scripts/stoicgen.js
@@ -0,0 +1,34 @@
+/*
+
+Created by:     GHXXST
+Website:        https://github.com/GHXXST/
+
+Project:        Stoic Quote Generator
+Description:    Generates new stoic quotes based on Marcus Aurelius' 'Meditations'.
+
+Requirements:   p5.js, rita.js
+
+*/
+
+
+var generator;
+const n = 2;  // N-gram length
+
+
+function setup() {
+    noCanvas();
+    
+    // First argument is N-gram length
+    generator = new RiMarkov(n, recognizeSentences=true, allowDuplicates=false);
+    generator.loadFrom('dictionaries/stoic-dictionary.txt');
+    // Button setup
+    var button = select('#button');
+    button.mousePressed(generate);
+}
+
+
+function generate() {
+    var text = generator.generateSentences(3);
+    var output = select('#gen0');
+    output.html(text[0]);
+}
